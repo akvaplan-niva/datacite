4bac130d43c7a2554c89684876680af9ac6cf008
cnrdh <cnrdh@users.noreply.github.com>
2022-08-24 14:25:35 +0200

    commit 0

---
+++ datacite_fetch_akvaplan.js
@@ -0,0 +1,44 @@
+const dois = "https://api.datacite.org/dois";
+
+const defaults = { "page[size]": 1000 };
+
+const url = (params) =>
+  new URL("?" + new URLSearchParams({ ...defaults, ...params }), dois);
+
+const fetchDatacite = async (params) =>
+  await fetch(url(params)).then((r) => r.json());
+
+const fetchQuery = async (query) => {
+  const { data } = await fetchDatacite({ query, affiliation: true });
+  return data;
+};
+
+const ndjson = (o) => console.log(JSON.stringify(o));
+
+const save = async (meta) => {
+  const {id}=meta;
+  const [prefix,...suf] = id.split("/");
+  
+  const dir = `dois/${encodeURIComponent(prefix)}`
+  await Deno.mkdir(dir, { recursive: true });
+
+  const filename = encodeURIComponent(suf.join("_")+".json");
+  Deno.writeTextFile(`${dir}/${filename}`, JSON.stringify(meta)+"\n");
+}
+
+
+const fetchAkvaplan = async () => {
+  const data = await fetchQuery("Akvaplan-niva");
+  const ids = new Set(data.map(({ id }) => id));
+
+  const data2 = await fetchQuery("creators.affiliation.name:Akvaplan-niva");
+  const add = data2.filter( ({id}) => !ids.has(id));
+  add.map(({id})=> ids.add(id));
+  return [...data,...add];
+}
+
+if (import.meta.main) {
+  const data = await fetchAkvaplan();
+  data.map(ndjson);
+  data.map(save);
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Akvaplan-niva datacite
+
+[akvaplan-niva/datacite](https://github.com/akvaplan-niva/datacite) contains Akvaplan-niva datasets from DataCite's [REST API](https://api.datacite.org/) v2.
+
+## CLI
+
+```sh
+$ datacite_fetch_akvaplan 
+``` 
+
+Planned: Automatic push to a [Deno Deploy](https://deno.com/deploy)
